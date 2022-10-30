7b12e117f68b276a0d72a117477292a1af22d6ef
[dietrichsimon] <simon.dietrich@gmail.com>
2022-10-30 19:25:15 +0100

    add function for creating a basic IRIS plotly map

---
+++ paris_bikes/mapping.py
@@ -0,0 +1,34 @@
+import geopandas as gpd
+import plotly.express as px
+
+
+
+def create_map(df: gpd.GeoDataFrame, var: str, width = 600, height = 400):
+
+    """Compute number of bike parking spots per IRIS.
+
+    Args:
+        df (gpd.GeoDataFrame): Dataframe with information to map.
+        var (str): Variable to map
+
+    Returns:
+        plotly map
+    """
+
+    # Create basemap
+    fig = px.choropleth(
+        df,
+        geojson=df.geometry,
+        locations=df.index,
+        projection="mercator",
+        color=var,
+        width=width, height=height
+        )
+
+    # Zoom map
+    fig.update_geos(fitbounds="locations", visible=True)
+
+    # Enhance layout
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    return fig

7082e0e2e00db559f83e0236e010be737a93660c
Joao Pinto Moura <joao.pinto.moura@ing.com>
2022-11-25 19:24:31 +0100

    allow switching between raw and normalized

--- paris_bikes/dash_application.py
+++ paris_bikes/dash_application.py
@@ -28,19 +28,36 @@ application.layout = html.Div(
         dcc.Location(id="url", refresh=False),
         html.H1(children="Paris Bikes"),
         html.Br(),
-        dcc.RadioItems(df.columns.drop(["geometry"]), "nb_pop", id="plot-column-input"),
+        dcc.RadioItems(df.columns.drop(["geometry"]), "nb_pop", id="plot-column-selector"),
+        html.Br(),
+        dcc.Checklist(["Normalize metrics by number of parking spots"], id="normalize-button"),
         html.Br(),
         dcc.Graph(id="map"),
     ]
 )
 
-# Link the plot-column-input with the map
+# Link the plot-column-selector with the output-map
 @application.callback(
     Output(component_id="map", component_property="figure"),
-    Input(component_id="plot-column-input", component_property="value"),
+    Input(component_id="plot-column-selector", component_property="value"),
 )
 def update_map(input_value):
     return create_map(df, input_value)
+
+
+# Link the normalize-button with the plot-column-selector
+@application.callback(
+    Output(component_id="plot-column-selector", component_property="options"),
+    Input(component_id="normalize-button", component_property="value"),
+)
+def update_plot_column_selector(input_value):
+    cols = df.columns.drop(["geometry"])
+    # If nothing is selected, use raw metrics
+    if input_value is None:
+        return [col for col in cols if "_normalized" not in col]
+    # Otherwise, use normalized metrics
+    else:
+        return [col for col in cols if "_normalized" in col]
 
 
 if __name__ == "__main__":
